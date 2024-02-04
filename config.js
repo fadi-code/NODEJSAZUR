35104a049ca6441607e3d093174978b460c76615
modeste kouassi <modeste.kouassi@workace.fr>
2024-02-04 15:04:15 +0100

    delete config

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: 'mongodb://social-bd:EvR94PkkhtI8WNgnYdV04qulAkYLK5qmlx1Me6oTdxqV4unmWQF5sNbzFxkgW03I8vb1dZSE5qEBACDbH9TeDw%3D%3D@social-bd.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@social-bd@',
-    secret: 'fadi',
-    Port : '3000',
-    azureStorageAccountName: 'socialfile',
-  azureStorageAccountKey: 'DefaultEndpointsProtocol=https;AccountName=socialfile;AccountKey=UPmVwugUoL8SvgNC+OtH8NjJAX1fF6LE6wfVJmZmW6xBTD2du1m1qUvkWGs5rODl9/0Ek/8yit0M+AStL88ZNQ==;EndpointSuffix=core.windows.net',
-  azureContainerName: 'image'
-  };
-  
