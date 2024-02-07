dcedca6f41ffe896953a6b57521e85bbf89e4793
fadi-code <121521880+fadi-code@users.noreply.github.com>
2024-02-07 21:02:48 +0100

    modif

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -6,7 +6,7 @@ const commentController = require('../controllers/commentController');
 router.post('/add', commentController.addComment);
 
 // Endpoint pour récupérer tous les commentaires d'un contenu
-router.get('/:contentId', commentController.getCommentsByContentId);
+router.get('/:PubId', commentController.getCommentsByContentId);
 
 router.delete('/delete', commentController.deleteCommentsByUserId);
 
